40b35cae98312fd0f448334d2510b4fb5aebcded
Max <ira6170724@gmail.com>
2019-08-08 17:15:41 -0500

    https://www.codewars.com/kata/my-head-is-at-the-wrong-end/train/javascript

--- solutions/8kyu/8 kyu My HeadIs At The Wrong End.js
+++ solutions/8kyu/8 kyu My HeadIs At The Wrong End.js
@@ -1,9 +1,12 @@
+//https://www.codewars.com/kata/my-head-is-at-the-wrong-end/train/javascript
+
 function fixTheMeerkat(arr) {
     let temp = arr[0];
     arr[0] = arr[2];
     arr[2] = temp;
  return arr;
 }
+
 function fixTheMeerkat(arr) {
   return arr.reverse();
 }
