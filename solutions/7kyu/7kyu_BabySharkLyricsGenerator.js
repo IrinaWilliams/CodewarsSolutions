648e1ae01f55d4a419fd6dc3353cdc94156cfbd7
Max <ira6170724@gmail.com>
2019-08-09 08:17:26 -0500

    added 7 kyu Baby shark lyrics generator

--- solutions/7kyu/7kyu_BabySharkLyricsGenerator.js
+++ solutions/7kyu/7kyu_BabySharkLyricsGenerator.js
@@ -1,6 +1,5 @@
 https://www.codewars.com/kata/baby-shark-lyrics-generator/train/javascript
 
-
 function babySharkLyrics(){
     const arr = ["Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"];
     return arr.map((name)=> `${name},${" doo".repeat(6)}\n`.repeat(3) + `${name}!\n`).join("") + "Run away,…";
