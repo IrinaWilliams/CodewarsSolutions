acfe2e3d59503e54b7c5158c292c3b6cd091670c
Irina Williams <ira6170724@gmail.com>
2019-09-27 11:08:09 -0500

    added 6 kyu Replace With Alphabet Position

---
+++ solutions/6kyu/6kyu_ReplaceWithAlphabetPosition.js
@@ -0,0 +1,45 @@
+https://www.codewars.com/kata/replace-with-alphabet-position/train/javascript
+
+/*Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+*/
+
+
+function alphabetPosition(text) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+     let splitedAlphabet = alphabet.split("");
+     let lowerText = text.toLowerCase();
+     let resArr = [];
+   
+     for (let i = 0; i < lowerText.length; i++) {
+       for (let q = 0; q < splitedAlphabet.length; q++) {
+         if (lowerText[i] === splitedAlphabet[q]) {
+           resArr.push(q + 1);
+         }
+       }
+     }
+     return resArr.join(' ');
+   }
+
+
+
+function alphabetPosition(text) {
+    return text
+      .toUpperCase()
+      .match(/[a-z]/gi)
+      .map( (c) => c.charCodeAt() - 64)
+      .join(' ');
+  }
+
+/*Test.assertEquals(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");*/
