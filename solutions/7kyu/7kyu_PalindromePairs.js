31807a8f701fed692ea19c6da1e5f297e0fdc043
Irina Williams <ira6170724@gmail.com>
2019-10-14 07:42:04 -0500

    added a new Kata 7 kyu Palindrome Pairs

--- solutions/7kyu/7kyu_PalindromePairs.js
+++ solutions/7kyu/7kyu_PalindromePairs.js
@@ -37,3 +37,11 @@ const palindromePairs = words => {
     }
     return res;
   }
+
+//   describe("Solution", function(){
+//     it("should return the right pairs", function(){
+//       Test.assertSimilar(palindromePairs(["bat", "tab", "cat"]), [[0, 1], [1, 0]]);
+//       Test.assertSimilar(palindromePairs(["dog", "cow", "tap", "god", "pat"]), [[0, 3], [2, 4], [3, 0], [4, 2]])
+//       Test.assertSimilar(palindromePairs(["abcd", "dcba", "lls", "s", "sssll"]), [[0, 1], [1, 0], [2, 4], [3, 2]])
+//     });
+//   });
