4c910d98a361973c6b43b58c785a296f44cdfcb5
Irina Williams <irinawilliams@MacBook-Pro-Irina.local>
2019-11-21 18:14:51 -0600

    added a new Kata 7 kyu Counting Duplicates

---
+++ solutions/6kyu/6kyu_CountingDuplicates.js
@@ -0,0 +1,35 @@
+/*
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+*/
+
+function duplicateCount(text){
+    if (text.length < 2) return 0;
+    text = text.toLowerCase().split('');
+    let res = 0;
+    let arr = [];
+    text.forEach((el, i) => {
+      if (text.indexOf(el) !== i && !arr.includes(el)) {
+        arr.push(el);
+        res++;
+  
+      }
+    })
+    return res;
+  }
+
+Test.assertEquals(duplicateCount(""), 0);
+Test.assertEquals(duplicateCount("abcde"), 0);
+Test.assertEquals(duplicateCount("aabbcde"), 2);
+Test.assertEquals(duplicateCount("aabBcde"), 2,"should ignore case");
+Test.assertEquals(duplicateCount("Indivisibility"), 1)
+Test.assertEquals(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
