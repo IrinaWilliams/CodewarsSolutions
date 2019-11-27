d127ca9e42e42df08a98855a6bf6e20b4a195442
Irina Williams <ira6170724@gmail.com>
2019-11-27 12:11:28 -0600

    added 6 kyu Sums of Parts

--- solutions/Random/TwoSum.js
+++ solutions/Random/TwoSum.js
@@ -10,4 +10,29 @@ const twoSum = (nums, target) => {
     return null;
 };
     
-    
+/// - [ ] two sum task - Write a function that return all pairs of 
+// numbers that sum to a target. Numbers cannot be use twice. 
+// [2,5,2] returns[[2,5]] because 5 can only be used once. 
+// f(0)=f(1)=f(2)=1; f(x) = f(x-1)+f(x-2)+f(x-3); input an x and return f(x) 
+
+let array = [2,4,5,3,2,1,5];
+
+function twoSum(num, target){
+  
+  let resArr = [];
+  let arr = [];
+  for(let i = 0; i < num.length-1; i++){
+    for(let j = 1; j < num.length; j++){
+      
+      if(num[i] + num[j] === target 
+         && arr.indexOf(i) === -1 
+         && arr.indexOf(j) === -1){
+        
+        resArr.push([num[i], num[j]]);
+        arr.push(i, j);
+      }
+    }    
+  }
+  return resArr;
+}
+console.log(twoSum(array, 7));
---
+++ solutions/Random/capitalizeFirstLetter.js
@@ -0,0 +1,9 @@
+//https://www.youtube.com/watch?v=VPiVncjGU5Q&feature=youtu.be
+
+"use strict";
+
+let names = ["Steven", "McKay", "sarah", "lynette", "David", "jose",
+"lana", "ALFRED", "samantha"];
+
+let titleCaseNames = names.map(name => name[0].toUpperCase()
+                    + name.slice(1));
