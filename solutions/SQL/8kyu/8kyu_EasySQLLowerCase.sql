a6666d497aa0c032d2d1b2042868e5e6fa3e6196
Irina Williams <ira6170724@gmail.com>
2019-12-14 07:19:59 -0600

    added a new solution 8kyu Easy SQL Lower Case

---
+++ solutions/SQL/8kyu/8kyu_EasySQLLowerCase.sql
@@ -0,0 +1,19 @@
+https://www.codewars.com/kata/easy-sql-lowercase/train/sql
+
+-- Given a demographics table in the following format:
+
+-- ** demographics table schema **
+
+-- id
+-- name
+-- birthday
+-- race
+-- you need to return the same table where all letters are lowercase in the race column.
+
+SELECT
+    id,
+    name,
+    birthday,
+    LOWER(race) as race
+FROM demographics;
+
